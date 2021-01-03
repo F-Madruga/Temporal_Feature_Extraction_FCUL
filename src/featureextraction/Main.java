759167007fb3d24b3e839cd6e7d51e70bfbc1e92
Francisco Madruga <madruga@pop-os.localdomain>
2021-01-03 18:09:43 +0000

    REFACTOR: Clean main.class and prints on main function

--- src/featureextraction/Main.java
+++ src/featureextraction/Main.java
@@ -2,18 +2,44 @@ package featureextraction;
 
 public class Main {
     public static void main(String[] args) {
+        long totalStart = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         DataFrame df = DataFrame.readCSV("./dataset.csv", ",", false);
         df.setColumnsNames(new String[] {"INDEX", "YEAR", "MONTH", "DAY", "AMOUNT", "BUYER", "SELLER", "CREDIT_CARD", "LATITUDE", "LONGITUDE"});
-        // System.out.println(df);
-        long start = System.currentTimeMillis();
-        // System.out.println(df.sum());
-        System.out.println(df.filter((columns, row) -> {
-            return (Integer) row.get(columns.get("SELLER")) == 302 && (Integer) row.get(columns.get("YEAR")) == 2018 && (Integer) row.get(columns.get("MONTH")) == 10;
-        }).mean(new String[]  {"AMOUNT"}));
-        System.out.println(df.filter((columns, row) -> {
-            return (Integer) row.get(columns.get("BUYER")) == 18;
-        }).mean(new String[]  {"AMOUNT"}));
-        //System.out.println(df.);
-        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start));
+        System.out.println("Time reading file: " + (System.currentTimeMillis() - start) + "\n");
+
+        int seller = 302;
+        int buyer = 18;
+        int year = 2018;
+        int month = 10;
+
+        // Alinia 1
+        start = System.currentTimeMillis();
+        System.out.println("The average amount of all past purchases of the seller " + seller + " is " +
+                        df.filter((columns, row) -> {
+                            return (Integer) row.get(columns.get("SELLER")) == seller;
+                        }).mean(new String[]  {"AMOUNT"}).getData().get(0).get(0)
+                );
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start) + "\n");
+
+        // Alinia 2
+        start = System.currentTimeMillis();
+        System.out.println("The average amount of all past purchases of the buyer " + buyer + " is " +
+                        df.filter((columns, row) -> {
+                            return (Integer) row.get(columns.get("BUYER")) == buyer;
+                        }).mean(new String[]  {"AMOUNT"}).getData().get(0).get(0)
+                );
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start) + "\n");
+
+        // Alinia 3
+        start = System.currentTimeMillis();
+        System.out.println("The average amount of all past purchases of the seller " + seller + " in the month " + month + " of " + year + " is " +
+                        df.filter((columns, row) -> {
+                            return (Integer) row.get(columns.get("SELLER")) == seller && (Integer) row.get(columns.get("YEAR")) == year && (Integer) row.get(columns.get("MONTH")) == month;
+                        }).mean(new String[]  {"AMOUNT"}).getData().get(0).get(0)
+                );
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - start) + "\n");
+
+        System.out.println("Total time elapsed: " + (System.currentTimeMillis() - totalStart));
     }
 }
